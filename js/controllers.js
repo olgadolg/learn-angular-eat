21abd293f5d1e086adaed92a24de6d42bb103fc2
Olga Dolgushina <olgadolg@gmail.com>
2015-07-13 22:19:17 +0600

    add geo api

--- js/controllers.js
+++ js/controllers.js
@@ -1,22 +1,57 @@
 var EatApp = angular.module("EatApp", []);
 
-EatApp.controller('EatCtrl', function($scope){
+EatApp.controller('EatCtrl', function ($scope) {
     $scope.eatList = [
-        {'name': 'nameZav1',
-         'descr': 'description1'},
-        {'name': 'nameZav2',
-            'descr': 'description2'}
+        {
+            'name': "traveler's coffee",
+            'descr': 'description1'
+        },
+        {
+            'name': 'dfzxvxvzxvzxvz',
+            'descr': 'description2'
+        }
     ];
 
-    $scope.actionAdd = function(){
+    $scope.getAddress = function(item){
+        //var allAddress = '';
+        var name = item.name;
+        var searchPlace = function(name) {
+            var request = {
+                query: name
+            };
+
+            var new_el = document.createElement('div');
+            service = new google.maps.places.PlacesService(new_el);
+            service.textSearch(request, takeAddress);
+        }(name);
+
+        function takeAddress(results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                item.address = (results[0].formatted_address).toString();
+                $scope.eatList.push(item);
+                $scope.$apply();
+            }
+        }
+    };
+
+    $scope.actionAdd = function () {
         var item = {};
         item.name = $scope.name;
         item.descr = $scope.descr;
-        $scope.eatList.push(item);
+        $scope.getAddress(item);
+        //item.address = $scope.getAddress($scope.name);
+        //$scope.eatList.push(item);
     };
 
-    $scope.actionDel = function(item){
+    $scope.actionDel = function (item) {
+        console.log('test');
         var index = $scope.eatList.indexOf(item);
         $scope.eatList.splice(index, 1);
     };
+    $scope.actionEdit = function (item, name, $event) {
+        var index = $scope.eatList.indexOf(item);
+        $scope.eatList[index][name] = $event.target.innerHTML;
+    };
+
 });
+
